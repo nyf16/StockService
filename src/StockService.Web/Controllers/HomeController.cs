a6ec2ab8ed8e8eacab02a6ce5b7b6141b18ae7cd
onur ferik <onurferik@gmail.com>
2019-10-20 12:49:46 +0300

    HomeController Update

--- src/StockService.Web/Controllers/HomeController.cs
+++ src/StockService.Web/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StockService.Domain.Product;
 using StockService.EF.Context;
 using StockService.Web.Models;
@@ -66,7 +68,91 @@ namespace StockService.Web.Controllers
             return View(model);
         }
 
-        
+        public IActionResult DetailsProduct(Guid id)
+        {
+            var product = _context.Product.Find(id);
+            return View(product);
+        }
+
+        public IActionResult EditProduct(Guid id)
+        {
+            var product = _context.Product.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Guid id, Product model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Product willUpdate = _context.Product.Find(model.Id);
+
+                    willUpdate.UrunAdi = model.UrunAdi;
+                    willUpdate.Kalite = model.Kalite;
+                    willUpdate.Kompozisyon = model.Kompozisyon;
+                    willUpdate.Miktar = model.Miktar;
+                    willUpdate.Musteri = model.Musteri;
+                    willUpdate.Renk = model.Renk;
+                    willUpdate.RezerveDurum = model.RezerveDurum;
+                    willUpdate.UrunKodu = model.UrunKodu;
+                    willUpdate.ModifiedBy = User.FindFirst(ClaimTypes.Name).Value;
+                    willUpdate.ModifiedById = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                    willUpdate.ModifiedDate = DateTime.UtcNow;
+
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Product", "Home");
+                }
+                catch (DBConcurrencyException ex)
+                {
+                    if (_context.Product.Find(id) == null)
+                        return NotFound();
+                    throw (ex);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult DeleteProduct(Guid id)
+        {
+            var product = _context.Product.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteProduct(Guid id, Product model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                Product product = _context.Product.Find(model.Id);
+
+                _context.Product.Remove(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("Product", "Home");
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (_context.Product.Find(model.Id) == null)
+                {
+                    return NotFound();
+                }
+                throw (ex);
+            }
+        }
+
         public IActionResult Privacy()
         {
             return View();
